afc98c0dc1523d9946388446931f5e10a372e96d
AkaCyberRat <andrejchuk01@gmail.com>
2022-10-08 00:00:21 +0700

    Fix StreamMetrics model.

--- App/Domain/NotifierService/Models/StreamMetrics.cs
+++ App/Domain/NotifierService/Models/StreamMetrics.cs
@@ -9,6 +9,22 @@ namespace App.Domain.NotifierService.Models
                              IReadOnlyDictionary<DateTime, string> titlesHistory,
                              IReadOnlyDictionary<DateTime, string> categoriesHistory)
         {
+            if(viewersCountHistory is null)
+            {
+                throw new ArgumentNullException(nameof(viewersCountHistory));
+            }
+            
+            if (titlesHistory is null)
+            {
+                throw new ArgumentNullException(nameof(titlesHistory));
+            }
+
+            if (categoriesHistory is null)
+            {
+                throw new ArgumentNullException(nameof(categoriesHistory));
+            }
+
+
             ViewersCountHistory = viewersCountHistory;
             TitlesHistory = titlesHistory;
             CategoriesHistory = categoriesHistory;
