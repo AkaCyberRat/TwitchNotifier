934bfd1d1319324cfa877912844d09443077ec43
AkaCyberRat <andrejchuk01@gmail.com>
2022-10-07 18:18:34 +0700

    Add StreamState model implementation.

--- App/Domain/NotifierService/Models/StreamMetrics.cs
+++ App/Domain/NotifierService/Models/StreamMetrics.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace App.Domain.NotifierService.Models
 {
     public class StreamMetrics
     {
+        public StreamMetrics(IReadOnlyDictionary<DateTime, int> viewersCountHistory,
+                             IReadOnlyDictionary<DateTime, string> titlesHistory,
+                             IReadOnlyDictionary<DateTime, string> categoriesHistory)
+        {
+            ViewersCountHistory = viewersCountHistory;
+            TitlesHistory = titlesHistory;
+            CategoriesHistory = categoriesHistory;
+        }
+
+        IReadOnlyDictionary<DateTime, int> ViewersCountHistory { get; }
+        IReadOnlyDictionary<DateTime, string> TitlesHistory { get; }
+        IReadOnlyDictionary<DateTime, string> CategoriesHistory { get; }
     }
 }
