e83e8cc2414c7ba8c6e383ec60a81bf72bfce568
AkaCyberRat <andrejchuk01@gmail.com>
2022-10-07 18:13:49 +0700

    Add Context model tests.

---
+++ AppTests/DomainTests/ModelTests/ContextTests.cs
@@ -0,0 +1,83 @@
+﻿using App.Domain.NotifierService.Models;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace AppTests.ModelTests
+{
+    [TestFixture]
+    internal class ContextTests
+    {
+
+        [TestCaseSource(nameof(ReturnsObjectCases))]
+        public void ConstructorInvoke_ReturnsObject(object parmsObj)
+        {
+            var parms = parmsObj as object[];
+
+            Assert.DoesNotThrow(() => new Context(  (StreamEvent)parms[0],
+                                                    (StreamState)parms[1],
+                                                    (StreamMetrics)parms[2]));
+        }
+
+
+        [TestCaseSource(nameof (ReturnsThrowCases))]
+        public void ConstructorInvoke_ReturnsThrow(object parmsObj)
+        {
+            var parms = parmsObj as object[];
+
+            Assert.Throws<ArgumentException>(() => new Context((StreamEvent)parms[0],
+                                                    (StreamState)parms[1],
+                                                    (StreamMetrics)parms[2]));
+        }
+
+
+        private static IEnumerable ReturnsObjectCases
+        {
+            get
+            {
+                yield return new TestCaseData(new object[] {
+                        StreamEvent.StreamStart,
+                        new StreamState("userName"),
+                        null
+                    } as object)
+                    .SetName("Correct params: StreamEvent, StreamState, null(StreamMetrics).");
+
+                yield return new TestCaseData(new object[] {
+                        StreamEvent.StreamStart,
+                        new StreamState("userName"),
+                        new StreamMetrics(
+                            new Dictionary<DateTime,int>().ToImmutableDictionary(),
+                            new Dictionary<DateTime,string>().ToImmutableDictionary(),
+                            new Dictionary<DateTime,string>().ToImmutableDictionary())
+                    } as object)
+                    .SetName("Correct params: StreamEvent, StreamState, StreamMetrics.");
+            }
+        }
+
+        private static IEnumerable ReturnsThrowCases
+        {
+            get
+            {
+                yield return new TestCaseData(new object[] {
+                        StreamEvent.StreamStart,
+                        null,
+                        null
+                    } as object)
+                    .SetName("Uncorrect params: StreamState(null).");
+
+                yield return new TestCaseData(new object[] {
+                        StreamEvent.StreamStart,
+                        null,
+                        new StreamMetrics(
+                            new Dictionary<DateTime,int>().ToImmutableDictionary(),
+                            new Dictionary<DateTime,string>().ToImmutableDictionary(),
+                            new Dictionary<DateTime,string>().ToImmutableDictionary())
+                    } as object)
+                    .SetName("Uncorrect params: StreamState(null).");
+            }
+        }
+
+    }
+}
