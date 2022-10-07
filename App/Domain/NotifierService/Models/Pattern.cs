666a9da3614198955fbce9ca139a285f0c7b7347
AkaCyberRat <andrejchuk01@gmail.com>
2022-10-07 17:34:40 +0700

    Fix Pattern model.

--- App/Domain/NotifierService/Models/Pattern.cs
+++ App/Domain/NotifierService/Models/Pattern.cs
@@ -1,4 +1,6 @@
-﻿namespace App.Domain.NotifierService.Models
+﻿using System;
+
+namespace App.Domain.NotifierService.Models
 {
     public class Pattern
     {
@@ -10,10 +12,20 @@
 
         public Pattern(string channel, string[] notifierTags, StreamEvent[] streamEvents = null, string[] categories = null)
         {
+            if (string.IsNullOrWhiteSpace(channel))
+                throw new ArgumentException($"'{nameof(channel)}' cannot be null or empty.", nameof(channel));   
+
+            if (notifierTags == null || notifierTags.Length == 0)
+                throw new ArgumentException($"'{nameof(notifierTags)}' cannot be null or empty.", nameof(notifierTags));
+
             Channel = channel;
             NotifierTags = notifierTags;
-            StreamEvents = streamEvents;
-            Categories = categories;
+
+
+            StreamEvents = streamEvents == null || streamEvents.Length == 0 ?
+                new StreamEvent[] { StreamEvent.StreamStart, StreamEvent.StreamStop } : streamEvents;
+
+            Categories = categories ?? new string[] { };
         }
     }
 }
